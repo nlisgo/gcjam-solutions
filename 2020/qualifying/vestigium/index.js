c04cdcb3999d2a392e66c422eb9665221d557b48
nlisgo <nathan@aeopus.com>
2021-03-25 16:33:56 +0000

    Add comments to vestigium solution

--- 2020/qualifying/vestigium/index.js
+++ 2020/qualifying/vestigium/index.js
@@ -1,7 +1,10 @@
+// Given the rows of a latin square return the trace (k),
+// rows with duplicates (r) and columns with duplicates (c).
 const solve = rows => {
     const cols = [];
     let r = 0;
     let c = 0;
+    // Prepare a columns array.
     for (let j = 0; j < rows.length; j++) {
         const col = [];
         for (let k = 0; k < rows.length; k++) {
@@ -9,7 +12,9 @@ const solve = rows => {
         }
         cols.push(col);
     }
+    // Calculate the trace.
     const k = calculateTrace(rows);
+    // For each row or column detect if duplicates found.
     for (let j = 0; j < rows.length; j++) {
         r += duplicateFound(rows[j]) ? 1 : 0;
         c += duplicateFound(cols[j]) ? 1 : 0;
@@ -17,6 +22,7 @@ const solve = rows => {
     return [k, r, c];
 };
 
+/// Given the rows of a latin square return the trace (k).
 const calculateTrace = latinSquares => {
     let trace = 0;
     for (let i = 0; i < latinSquares.length; i++) {
@@ -25,35 +31,43 @@ const calculateTrace = latinSquares => {
     return trace;
 };
 
+// Detect if a duplicate is found in an array.
 const duplicateFound = values => values.length > values.filter((v, i, a) => a.indexOf(v) === i).length;
 
+// If stdin is detected.
 if (!Boolean(process.stdin.isTTY)) {
 	const readline = require('readline');
 
     let inputs = [];
 
     const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    terminal: false,
+        input: process.stdin,
+        output: process.stdout,
+        terminal: false,
     });
 
+    // Read each line into the array inputs.
     rl.on('line', line => {
         inputs.push(line);
     }).on('close', () => {
         let x = 0;
+        // Discard the first line of inputs.
         inputs = inputs.slice(1);
         inputs.forEach((data, i) => {
+            // If the line contains a single integer.
             if (Number.parseInt(data) == data) {
                 x++;
+                // Collect the rows of the latin square.
                 const rows = [];
                 for (let j = 1; j <= inputs[i]; j++) {
                     rows.push(inputs[i+j].split(' '));
                 }
+                // Output the case after passing rows to solve method above.
                 console.log(`Case #${x}: ${solve(rows).join(' ')}`);
             }
         });
     });
 } else {
+    // Export as module for tests to work on solve method.
 	module.exports = solve;
 }
