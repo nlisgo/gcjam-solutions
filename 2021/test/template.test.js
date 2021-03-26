d00b876e6f6b17b73646efb0ea10eca66bab84d2
nlisgo <nathan@aeopus.com>
2021-03-26 13:15:39 +0000

    Revert "Try this out"
    This reverts commit 33ac1dce005f3418f3f6f683094de0b6ecda294e.

--- 2021/test/template.test.js
+++ 2021/test/template.test.js
@@ -1,27 +1,27 @@
-const { solve, solveInputs } = require('../template/index');
+const template = require('../template/index');
 
 describe('solve', () => {
     test('one', () => {
-        expect(solve('one')).toBe('one');
+        expect(template.solve('one')).toBe('one');
     });
     
     test('two', () => {
-        expect(solve('two')).toBe('two');
+        expect(template.solve('two')).toBe('two');
     });
     
     test('three', () => {
-        expect(solve('three')).toBe('three');
+        expect(template.solve('three')).toBe('three');
     });
     
     test('four', () => {
-        expect(solve('four')).toBe('four');
+        expect(template.solve('four')).toBe('four');
     });
 });
 
 describe('solveInputs', () => {
     test('sample.in', () => {
         expect(
-            solveInputs(`3
+            template.solveInputs(`3
 one
 two
 three`.split(/\r?\n/))
