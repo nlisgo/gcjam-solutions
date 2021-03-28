ece7237f3ab7529af34eaf80fcd3f74067b0a5aa
nlisgo <nathan@aeopus.com>
2021-03-28 03:09:12 +0100

    fix tests

--- 2021/test/qualifying/moons-and-umbrellas.test.js
+++ 2021/test/qualifying/moons-and-umbrellas.test.js
@@ -1,20 +1,20 @@
-const moon_and_umbrellas = require('../../../qualifying/moons-and-umbrellas/index');
+const moon_and_umbrellas = require('../../qualifying/moons-and-umbrellas/index');
 
 describe('solve', () => {
-  test('[2, 3, CJ?CC?]', () => {
-    expect(moon_and_umbrellas.solve([2, 3, 'CJ?CC?'])).toBe(5);
+  test('2, 3, CJ?CC?', () => {
+    expect(moon_and_umbrellas.solve(2, 3, 'CJ?CC?')).toBe(5);
   });
 
-  test('[4, 2, CJCJ]', () => {
-    expect(moon_and_umbrellas.solve([4, 2, 'CJCJ'])).toBe(10);
+  test('4, 2, CJCJ', () => {
+    expect(moon_and_umbrellas.solve(4, 2, 'CJCJ')).toBe(10);
   });
 
-  test('[4, 2, CJCJ]', () => {
-    expect(moon_and_umbrellas.solve([2, 3, 'CJ?CC?'])).toBe(1);
+  test('2, 3, C?J', () => {
+    expect(moon_and_umbrellas.solve(1, 3, 'C?J')).toBe(1);
   });
 
-  test('[2 5 ??J???]', () => {
-    expect(moon_and_umbrellas.solve([2, 5, '??J???'])).toBe(0);
+  test('2 5 ??J???', () => {
+    expect(moon_and_umbrellas.solve(2, 5, '??J???')).toBe(0);
   });
 });
 
