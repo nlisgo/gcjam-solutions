f7e672c9c39baddcc9900d11f158a6d9410dbb2e
Nathan Lisgo <nathan@aeopus.com>
2021-03-31 21:54:21 +0100

    Moon and umbrellas bonus (#3)

--- 2021/qualifying/moons-and-umbrellas/index.js
+++ 2021/qualifying/moons-and-umbrellas/index.js
@@ -1,46 +1,107 @@
 Number.prototype.toCase = function () {
-  return this.valueOf()+1;
+    return this.valueOf()+1;
 };
 
-const solve = (x = 0, y = 0, s = '') => {
-  s = s.replace(/\?/g, '');
-  var  cost = ((s.match(/CJ/g) || []).length * x) + ((s.match(/JC/g) || []).length * y);
+const solve = (costCJ, costJC, s) => {
+    const splits = splitS(s).map(split => {
+        return cheapestOption(options(split), costCJ, costJC)[1];
+    });
 
-  return cost;
+    return splits.reduce((a, b) => a + b, 0);
 };
+
+const splitS = s => {
+    sArray = s.split('');
+    parts = [];
+    let part = '';
+    for (let i = 0; i < sArray.length; i++) {
+        part += sArray[i];
+
+        if (i === sArray.length - 1 && part.length > 1) {
+            parts.push(part);
+        } else if (sArray[i] !== '?' && sArray[i + 1] === '?') {
+            parts.push(part);
+            part = sArray[i];
+        } else if (sArray[i] === '?' && sArray[i + 1] !== '?') {
+            part += sArray[i + 1];
+            parts.push(part);
+            part = '';
+        }
+    }
+
+    return parts;
+};
+
+const options = s => {
+    const len = (s.match(/\?/g) || []).length;
+
+    return [
+        s.replace(/\?+/g, 'C'.repeat(len)),
+        s.replace(/\?+/g, 'J'.repeat(len)),
+        s.replace(/\?+/g, 'CJ'.repeat(len).substring(0, len)),
+        s.replace(/\?+/g, 'JC'.repeat(len).substring(0, len)),
+    ].filter((v, i, a) => a.indexOf(v) === i);
+};
+
+const cheapestOption = (options, costCJ, costJC) => {
+    let cheapO = options[0];
+    let cheap = optionCost(cheapO, costCJ, costJC);
+
+    if (options.length > 1) {
+        for (let i = 1; i < options.length; i++) {
+            let compareCost = optionCost(options[i], costCJ, costJC);
+            if (compareCost < cheap) {
+                cheap = compareCost;
+                cheapO = options[i];
+            }
+        }
+    }
+    return [cheapO, cheap];
+};
+
+const optionCost = (option, costCJ, costJC) => ((option.match(/CJ/g) || []).length * costCJ) + ((option.match(/JC/g) || []).length * costJC);
 
 const solveInputs = inputs => {
-  const cases = [];
-  inputs.slice(1).forEach((data, i) => {
-    var parts = data.split(' ');
-    cases.push(`Case #${i.toCase()}: ${solve(parts[0], parts[1], parts[2])}`);
-  });
-  return cases;
+    const cases = [];
+    inputs.slice(1).forEach((data, i) => {
+        cases.push(`Case #${i.toCase()}: ${solve(...data.split(' ').map(i => {
+            if (!isNaN(i)) {
+                return Number(i);
+            }
+
+            return i;
+        }))}`);
+    });
+    return cases;
 };
 
 const handleStdin = () => {
-  const readline = require('readline');
+    const readline = require('readline');
 
-  const inputs = [];
+    const inputs = [];
 
-  const rl = readline.createInterface({
-    input: process.stdin
-  });
-
-  rl.on('line', line => {
-    inputs.push(line);
-  }).on('close', () => {
-    solveInputs(inputs).forEach(out => {
-      console.log(out);
+    const rl = readline.createInterface({
+        input: process.stdin
     });
-  });
+
+    rl.on('line', line => {
+        inputs.push(line);
+    }).on('close', () => {
+        solveInputs(inputs).forEach(out => {
+            console.log(out);
+        });
+    });
 };
 
 if (!Boolean(process.stdin.isTTY)) {
-  handleStdin();
+    handleStdin();
 }
 
 module.exports = {
-  solve,
-  solveInputs,
+    solve,
+    solveInputs,
+    options,
+    splitS,
+    optionCost,
+    cheapestOption,
 };
--- 2021/test/qualifying/moons-and-umbrellas.test.js
+++ 2021/test/qualifying/moons-and-umbrellas.test.js
@@ -1,39 +1,155 @@
-const moon_and_umbrellas = require('../../qualifying/moons-and-umbrellas/index');
+const moonsAndUmbrellas = require('../../qualifying/moons-and-umbrellas/index');
 
 describe('solve', () => {
-  test('2, 3, CJ?CC?', () => {
-    expect(moon_and_umbrellas.solve(2, 3, 'CJ?CC?')).toBe(5);
-  });
+    test('2, 3, CJ?CC?', () => {
+        expect(moonsAndUmbrellas.solve(2, 3, 'CJ?CC?')).toBe(5);
+    });
 
-  test('4, 2, CJCJ', () => {
-    expect(moon_and_umbrellas.solve(4, 2, 'CJCJ')).toBe(10);
-  });
+    test('4, 2, CJCJ', () => {
+        expect(moonsAndUmbrellas.solve(4, 2, 'CJCJ')).toBe(10);
+    });
 
-  test('2, 3, C?J', () => {
-    expect(moon_and_umbrellas.solve(1, 3, 'C?J')).toBe(1);
-  });
+    test('2, 3, C?J', () => {
+        expect(moonsAndUmbrellas.solve(1, 3, 'C?J')).toBe(1);
+    });
 
-  test('2 5 ??J???', () => {
-    expect(moon_and_umbrellas.solve(2, 5, '??J???')).toBe(0);
-  });
+    test('2, 5, ??J???', () => {
+        expect(moonsAndUmbrellas.solve(2, 5, '??J???')).toBe(0);
+    });
+
+    test('2, -5, ??J??', () => {
+        expect(moonsAndUmbrellas.solve(2, -5, '??J??')).toBe(-8);
+    });
+
+    test('4, 3, ?JC?JJJCCCC', () => {
+        expect(moonsAndUmbrellas.solve(4, 3, '?JC?JJJCCCC')).toBe(10);
+    });
+
+    test('2, -5, C?C', () => {
+        expect(moonsAndUmbrellas.solve(2, -5, 'C?C')).toBe(-3);
+    });
+
+    test('-5, -3, C?C', () => {
+        expect(moonsAndUmbrellas.solve(-5, -3, 'C?C')).toBe(-8);
+    });
+
+    test('1, 1, C?C', () => {
+        expect(moonsAndUmbrellas.solve(1, 1, 'C?C')).toBe(0);
+    });
+
+    test('-1, 2, C?C', () => {
+        expect(moonsAndUmbrellas.solve(-1, 2, 'C?C')).toBe(0);
+    });
+
+    test('-1, -2, J??C', () => {
+        expect(moonsAndUmbrellas.solve(-1, -2, 'J??C')).toBe(-5);
+    });
+
+    test('6, -1, J??C', () => {
+        expect(moonsAndUmbrellas.solve(6, -1, 'J??C')).toBe(-1);
+    });
+
+    test('6, -4, J??C', () => {
+        expect(moonsAndUmbrellas.solve(6, -4, 'J??C')).toBe(-4);
+    });
+
+    test('-2, -3, ??', () => {
+        expect(moonsAndUmbrellas.solve(-2, -3, '??')).toBe(-3);
+    });
+
+    test('1, 3, ?????', () => {
+        expect(moonsAndUmbrellas.solve(1, 3, '?????')).toBe(0);
+    });
+
+    test('-10, 3, ?????', () => {
+        expect(moonsAndUmbrellas.solve(-10, 3, '?????')).toBe(-14);
+    });
 });
 
 describe('solveInputs', () => {
-  test('sample.in', () => {
-    expect(
-      moon_and_umbrellas.solveInputs(`4
+    test('sample.in', () => {
+        expect(
+            moonsAndUmbrellas.solveInputs(`4
 2 3 CJ?CC?
 4 2 CJCJ
 1 3 C?J
-2 5 ??J???`.split(/\r?\n/))
-    )
-      .toStrictEqual(
-        [
-          'Case #1: 5',
-          'Case #2: 10',
-          'Case #3: 1',
-          'Case #4: 0'
-        ]
-      );
-  });
+2 5 ??J???
+2 -5 ??J??`.split(/\r?\n/))
+        )
+        .toStrictEqual(
+            [
+                'Case #1: 5',
+                'Case #2: 10',
+                'Case #3: 1',
+                'Case #4: 0',
+                'Case #5: -8',
+            ]
+        );
+    });
+});
+
+describe('optimise moons and umbrellas', () => {
+    const optimiseS = (s, costCJ, costJC) => {
+        return moonsAndUmbrellas.splitS(s).map(split => moonsAndUmbrellas.cheapestOption(moonsAndUmbrellas.options(split), costCJ, costJC)[0]).join('|').replace(/\|./g, '');
+    };
+
+    test('CJ?CC?, 2, 3', () => {
+        expect(optimiseS('CJ?CC?', 2, 3)).toBe('CJCCCC');
+    });
+
+    test('CJCJ, 4, 2', () => {
+        expect(optimiseS('CJCJ', 4, 2)).toBe('CJCJ');
+    });
+
+    test('C?J, 2, 3', () => {
+        expect(optimiseS('C?J', 1, 3)).toBe('CCJ');
+    });
+
+    test('??J???, 2, 5', () => {
+        expect(optimiseS('??J???', 2, 5)).toBe('JJJJJJ');
+    });
+
+    test('??J??, 2, -5', () => {
+        expect(optimiseS('??J??', 2, -5)).toBe('JCJCC');
+    });
+
+    test('C?C, 2, -5', () => {
+        expect(optimiseS('C?C', 2, -5)).toBe('CJC');
+    });
+
+    test('C?C, -5, -3', () => {
+        expect(optimiseS('C?C', -5, -3)).toBe('CJC');
+    });
+
+    test('C?C, -1, 2', () => {
+        expect(optimiseS('C?C', -1, 2)).toBe('CCC');
+    });
+
+    test('J??C, -1, -2', () => {
+        expect(optimiseS('J??C', -1, -2)).toBe('JCJC');
+    });
+
+    test('J??C, 6, -1', () => {
+        expect(optimiseS('J??C', 6, -1)).toBe('JCCC');
+    });
+
+    test('J??C, 6, -4', () => {
+        expect(optimiseS('J??C', 6, -4)).toBe('JCCC');
+    });
+
+    test('J??C, 6, -7', () => {
+        expect(optimiseS('J??C', 6, -7)).toBe('JCJC');
+    });
+
+    test('??, -2, -3', () => {
+        expect(optimiseS('??', -2, -3)).toBe('JC');
+    });
+
+    test('?????, 1, 3', () => {
+        expect(optimiseS('?????', 1, 3)).toBe('CCCCC');
+    });
+
+    test('?????, -10, 3', () => {
+        expect(optimiseS('?????', -10, 3)).toBe('CJCJC');
+    });
 });
