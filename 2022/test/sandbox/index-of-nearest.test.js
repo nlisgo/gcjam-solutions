4adf468ac09db32d1f0c9de35216127e5a18a9ae
nlisgo <nathan@aeopus.com>
2022-08-23 17:37:28 +0100

    Amend tests

--- 2022/test/sandbox/index-of-nearest.test.js
+++ 2022/test/sandbox/index-of-nearest.test.js
@@ -4,35 +4,34 @@ describe('splitInput', () => {
     test('sample.in', () => {
         expect(
             indexOfNearest.splitInput(`3
-one
-two
-three`.split(/\r?\n/))
+4 0 spanner
+spanner spanner wrench hammer
+4 3 spanner
+spanner spanner wrench hammer
+7 3 pliers
+hammer screwdriver pliers wrench spanner pliers pliers`.split(/\r?\n/))
         )
         .toStrictEqual(
             [
-                'one',
-                'two',
-                'three',
+                [0, 'spanner', ['spanner', 'spanner', 'wrench', 'hammer']],
+                [3, 'spanner', ['spanner', 'spanner', 'wrench', 'hammer']],
+                [3, 'pliers', ['hammer', 'screwdriver', 'pliers', 'wrench', 'spanner', 'pliers', 'pliers']],
             ]
         );
     });
 });
 
 describe('solve', () => {
-    test('one', () => {
-        expect(indexOfNearest.solve('one')).toBe('one');
+    test("[0, 'spanner', ['spanner', 'spanner', 'wrench', 'hammer']]", () => {
+        expect(indexOfNearest.solve([0, 'spanner', ['spanner', 'spanner', 'wrench', 'hammer']])).toBe(0);
     });
 
-    test('two', () => {
-        expect(indexOfNearest.solve('two')).toBe('two');
+    test("[3, 'spanner', ['spanner', 'spanner', 'wrench', 'hammer']]", () => {
+        expect(indexOfNearest.solve([3, 'spanner', ['spanner', 'spanner', 'wrench', 'hammer']])).toBe(1);
     });
 
-    test('three', () => {
-        expect(indexOfNearest.solve('three')).toBe('three');
-    });
-
-    test('four', () => {
-        expect(indexOfNearest.solve('four')).toBe('four');
+    test("[3, 'pliers', ['hammer', 'screwdriver', 'pliers', 'wrench', 'spanner', 'pliers', 'pliers']]", () => {
+        expect(indexOfNearest.solve([3, 'pliers', ['hammer', 'screwdriver', 'pliers', 'wrench', 'spanner', 'pliers', 'pliers']])).toBe(2);
     });
 });
 
@@ -40,15 +39,18 @@ describe('solveInputs', () => {
     test('sample.in', () => {
         expect(
             indexOfNearest.solveInputs(`3
-one
-two
-three`.split(/\r?\n/))
+4 0 spanner
+spanner spanner wrench hammer
+4 3 spanner
+spanner spanner wrench hammer
+7 3 pliers
+hammer screwdriver pliers wrench spanner pliers pliers`.split(/\r?\n/))
         )
         .toStrictEqual(
             [
-                'Case #1: one',
-                'Case #2: two',
-                'Case #3: three',
+                'Case #1: 0',
+                'Case #2: 1',
+                'Case #3: 2',
             ]
         );
     });
