bcaad3536fbee3f3161b12b95c3c0f5f53a55ad5
nlisgo <n.lisgo@elifesciences.org>
2023-04-15 19:40:13 +0100

    solved for sample but WA on large set

--- 2023/round-a/ascii-art/index.js
+++ 2023/round-a/ascii-art/index.js
@@ -3,11 +3,25 @@
  */
 const splitInput = input => input.slice(1).map(Number);
 
+const nearestTriangularNumberBelow = (n) => {
+    let triangularNumber = Math.floor((Math.sqrt(8 * n + 1) - 1) / 2);
+    if ((triangularNumber * (triangularNumber + 1)) / 2 > n) {
+        triangularNumber--;
+    }
+    return (triangularNumber * (triangularNumber + 1)) / 2;
+}
+
+const findTriangularNumberPosition = (T) => Math.ceil((-1 + Math.sqrt(1 + 8 * T)) / 2);
+
 /**
  * Accepts a single input case and returns the result as a string.
  */
 const solve = input => {
-    return input;
+    const nearest = nearestTriangularNumberBelow(input / 26);
+    const r = input - nearest * 26;
+    const pos = findTriangularNumberPosition(nearest) + 1;
+    const letterIndex = Math.ceil(r / pos);
+    return 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')[letterIndex - 1];
 };
 
 /**
@@ -43,6 +57,7 @@ if (!Boolean(process.stdin.isTTY)) {
  * Export all functions that we want to test.
  */
 module.exports = {
+    nearestTriangularNumberBelow,
 	splitInput,
 	solve,
 	solveInputs,
--- 2023/test/round-a/ascii-art.test.js
+++ 2023/test/round-a/ascii-art.test.js
@@ -16,6 +16,36 @@ describe('splitInput', () => {
     });
 });
 
+describe('nearestTriangularNumberBelow', () => {
+    test('0.5', () => {
+        expect(asciiArt.nearestTriangularNumberBelow(0.5)).toBe(0);
+    });
+
+    test('1', () => {
+        expect(asciiArt.nearestTriangularNumberBelow(1)).toBe(1);
+    });
+
+    test('2', () => {
+        expect(asciiArt.nearestTriangularNumberBelow(2)).toBe(1);
+    });
+
+    test('3', () => {
+        expect(asciiArt.nearestTriangularNumberBelow(3)).toBe(3);
+    });
+
+    test('7', () => {
+        expect(asciiArt.nearestTriangularNumberBelow(7)).toBe(6);
+    });
+
+    test('9', () => {
+        expect(asciiArt.nearestTriangularNumberBelow(9)).toBe(6);
+    });
+
+    test('13', () => {
+        expect(asciiArt.nearestTriangularNumberBelow(13)).toBe(10);
+    });
+});
+
 describe('solve', () => {
     test('5', () => {
         expect(asciiArt.solve(5)).toBe('E');
