ccf44151de4d8565612cf837ce649ba57d151da6
nlisgo <n.lisgo@elifesciences.org>
2023-04-15 15:39:45 +0100

    Create boilerplate code for 2023 round A colliding encoding and amend tests and sample.in

---
+++ 2023/test/round-a/colliding-encoding.test.js
@@ -0,0 +1,68 @@
+const collidingEncoding = require('../../round-a/colliding-encoding/index');
+
+describe('splitInput', () => {
+    test.only('sample.in', () => {
+        expect(
+            collidingEncoding.splitInput(`2
+0 1 2 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+4
+ABC
+BC
+BCD
+CDE
+0 1 2 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+3
+CDE
+DEF
+EFG
+`.split(/\r?\n/))
+        )
+        .toStrictEqual(
+            [
+                [
+                    [0, 1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+                    ['ABC', 'BC', 'BCD', 'CDE'],
+                ],
+                [
+                    [0, 1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+                    ['CDE', 'DEF', 'EFG'],
+                ],
+            ]
+        );
+    });
+});
+
+describe('solve', () => {
+    test('[0, 1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3], [ABC, BC, BCD, CDE]', () => {
+        expect(collidingEncoding.solve([0, 1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3], ['ABC', 'BC', 'BCD', 'CDE'])).toBe('NO');
+    });
+
+    test('[0, 1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3], [CDE, DEF, EFG]', () => {
+        expect(collidingEncoding.solve([0, 1, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3], ['CDE', 'DEF', 'EFG'])).toBe('YES');
+    });
+});
+
+describe('solveInputs', () => {
+    test('sample.in', () => {
+        expect(
+            collidingEncoding.solveInputs(`2
+0 1 2 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+4
+ABC
+BC
+BCD
+CDE
+0 1 2 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+3
+CDE
+DEF
+EFG`.split(/\r?\n/))
+        )
+        .toStrictEqual(
+            [
+                'Case #1: NO',
+                'Case #2: YES',
+            ]
+        );
+    });
+});
